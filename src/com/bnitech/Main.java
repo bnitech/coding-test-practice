d056a36684a411c894090952c29d9fdbffdd51d6
bnitech <bnitech88@gmail.com>
2020-12-16 07:12:14 +0900

    1423. Maximum Points You Can Obtain from Cards

--- src/com/bnitech/Main.java
+++ src/com/bnitech/Main.java
@@ -1,14 +1,13 @@
 package com.bnitech;
 
-import com.bnitech.leetcode.Q763.Solution;
-
-import java.util.List;
+import com.bnitech.leetcode.Q1423.Solution;
 
 public class Main {
 
     public static void main(String[] args) {
-        String input = "ababcbacadefegdehijhklij";
-        List<Integer> result = Solution.partitionLabels(input);
-        System.out.println(result);
+        int[] input = {9,7,7,9,7,7,9};
+        int k = 7;
+        int score = Solution.maxScore(input, k);
+        System.out.println(score);
     }
 }
