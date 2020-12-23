15410b627c9c54af97b0b37a119a3d2a3d57a151
bnitech <bnitech88@gmail.com>
2020-12-23 21:26:35 +0900

    136. Single Number

---
+++ src/com/bnitech/leetcode/Q_136/Solution2.java
@@ -0,0 +1,16 @@
+package com.bnitech.leetcode.Q_136;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// 136. Single Number
+public class Solution2 {
+    public static int singleNumber(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            if (numSet.contains(num)) numSet.remove(num);
+            else numSet.add(num);
+        }
+        return numSet.iterator().next();
+    }
+}
