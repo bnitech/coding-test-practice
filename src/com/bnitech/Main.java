60293c135ead7a103928e5902f505deda606b8d3
bnitech <bnitech88@gmail.com>
2020-12-28 21:42:07 +0900

    54. Spiral Matrix

--- src/com/bnitech/Main.java
+++ src/com/bnitech/Main.java
@@ -1,14 +1,14 @@
 package com.bnitech;
 
-import com.bnitech.leetcode.Q_238.Solution;
+import com.bnitech.leetcode.Q_54.Solution;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] input = {1, 2, 3, 4};
-        int[] results = Solution.productExceptSelf(input);
-        Arrays.stream(results).forEach(System.out::println);
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        List<Integer> integerList = Solution.spiralOrder(matrix);
+        integerList.forEach(System.out::print);
     }
 }
