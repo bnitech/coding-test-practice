5ed2e68cc41a253f22fdf519a6dacba9aad160ea
bnitech <bnitech88@gmail.com>
2021-01-11 20:07:37 +0900

    103. Binary Tree Zigzag Level Order Traversal

--- src/com/bnitech/leetcode/Q_103/Solution.java
+++ src/com/bnitech/leetcode/Q_103/Solution.java
@@ -1,6 +1,5 @@
 package com.bnitech.leetcode.Q_103;
 
-import com.bnitech.leetcode.Pair;
 import com.bnitech.leetcode.TreeNode;
 
 import java.util.*;
@@ -10,47 +9,35 @@ public class Solution {
     private static final int TO_RIGHT = 0, TO_LEFT = 1;
 
     public static List<List<Integer>> zigzagLevelOrder(final TreeNode root) {
-        if(root == null) return Collections.emptyList();
+        if (root == null) return Collections.emptyList();
 
         List<List<Integer>> valueListList = new ArrayList<>();
-        valueListList.add(Collections.singletonList(root.val));
 
-        Queue<Pair<TreeNode, Integer>> pairQueue = new LinkedList<>();
-        pairQueue.offer(new Pair<>(root, TO_LEFT));
+        Queue<TreeNode> treeNodeQueue = new LinkedList<>();
+        treeNodeQueue.offer(root);
 
-        while (!pairQueue.isEmpty()) {
-            Pair<TreeNode, Integer> currentPair = pairQueue.poll();
-            TreeNode currentTreeNode = currentPair.getFront();
-            if(currentTreeNode == null) continue;
-            int currentDir = currentPair.getRear();
-
-            if (currentTreeNode.left == null && currentTreeNode.right == null) {
-                continue;
-            }
+        int depth = 0;
+        while (!treeNodeQueue.isEmpty()) {
+            int size = treeNodeQueue.size();
 
             List<Integer> valueList = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = treeNodeQueue.poll();
+                if (treeNode == null) continue;
 
-            if (currentDir == TO_RIGHT) {
-                if (currentTreeNode.left != null) {
-                    valueList.add(currentTreeNode.left.val);
-                    pairQueue.offer(new Pair<>(currentTreeNode.left, TO_LEFT));
-                }
-                if (currentTreeNode.right != null) {
-                    valueList.add(currentTreeNode.right.val);
-                    pairQueue.offer(new Pair<>(currentTreeNode.right, TO_LEFT));
-                }
-            } else {
-                if (currentTreeNode.right != null) {
-                    valueList.add(currentTreeNode.right.val);
-                    pairQueue.offer(new Pair<>(currentTreeNode.right, TO_RIGHT));
-                }
-                if (currentTreeNode.left != null) {
-                    valueList.add(currentTreeNode.left.val);
-                    pairQueue.offer(new Pair<>(currentTreeNode.left, TO_RIGHT));
-                }
+                valueList.add(treeNode.val);
+                treeNodeQueue.offer(treeNode.left);
+                treeNodeQueue.offer(treeNode.right);
             }
 
-            if (valueList.size() > 0) valueListList.add(valueList);
+            if (!valueList.isEmpty()) {
+                if (depth % 2 == TO_LEFT) {
+                    Collections.reverse(valueList);
+                }
+                valueListList.add(valueList);
+            }
+
+            depth++;
         }
 
         return valueListList;
