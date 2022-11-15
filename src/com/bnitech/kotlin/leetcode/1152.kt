942b19f909de5783e0a6a9156b3fd4fbaae06ab8
bnitech <bnitech88@gmail.com>
2022-11-15 20:00:49 +0900

    leetcode 1152 #1

--- src/com/bnitech/kotlin/leetcode/1152.kt
+++ src/com/bnitech/kotlin/leetcode/1152.kt
@@ -1,30 +1,39 @@
 package com.bnitech.kotlin.leetcode
 
+data class Log(val time: Int, val name: String, val website: String)
+
 fun mostVisitedPattern(username: Array<String>, timestamp: IntArray, website: Array<String>): List<String> {
-    val visitedWebsites = mutableMapOf<String, ArrayList<String>>()
+    val logs = arrayListOf<Log>()
     for (i in timestamp.indices) {
-        val list = visitedWebsites.getOrDefault(username[i], arrayListOf())
-        list.add(website[i])
-        visitedWebsites[username[i]] = list
+        logs.add(Log(timestamp[i], username[i], website[i]))
+    }
+    logs.sortWith(Comparator { a, b -> a.time.compareTo(b.time) })
+
+    val visitedWebsites = mutableMapOf<String, ArrayList<String>>()
+    for (log in logs) {
+        val list = visitedWebsites.getOrDefault(log.name, arrayListOf())
+        list.add(log.website)
+        visitedWebsites[log.name] = list
     }
 
-    val score = mutableMapOf<String, Int>()
+    val score = mutableMapOf<String, Set<String>>()
     for (user in visitedWebsites.keys) {
         val websites = visitedWebsites[user]!!
         val listSize = websites.size
+
         for (i in 0 until listSize - 2) {
             for (j in i + 1 until listSize - 1) {
                 for (k in j + 1 until listSize) {
                     val key = websites[i] + " " + websites[j] + " " + websites[k]
-                    score[key] = score.getOrDefault(key, 0) + 1
+                    score[key] = score.getOrDefault(key, mutableSetOf()).plus(user)
                 }
             }
         }
     }
 
-    val result = score.toList().sortedWith { a, b ->
-        if (a.second == b.second) a.first.compareTo(b.first) else a.second.compareTo(b.second) * -1
-    }[0]
+    val result = score.toList().sortedWith(Comparator { a, b ->
+        if (a.second.size == b.second.size) a.first.compareTo(b.first) else a.second.size.compareTo(b.second.size) * -1
+    })[0]
 
     return result.first.split(" ")
 }
